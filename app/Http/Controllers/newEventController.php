8583a898c22b0d19e49789422694ac136c96a4b4
ad.bme <adil.bouamrioune@gmail.com>
2023-06-21 08:56:31 +0200

    updates finally work!

--- app/Http/Controllers/newEventController.php
+++ app/Http/Controllers/newEventController.php
@@ -70,9 +70,23 @@ class newEventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $newEvent = newEvent::find($id);
+        $input = $request->only([
+            'name_surname_author_new_event',
+            'title_new_event',
+            'description_new_event',
+            'join_file_new_event',
+            'join_link_new_event',
+            'date_new_event',
+            'hour_new_event',
+            'room_new_event',
+            'plan_room_new_event',
+            'num_disposition_new_event'
+        ]);
+        $newEvent->update($input);
+        return redirect('newEvent')->with('flash_message', 'New Event updated !');
     }
 
     /**
