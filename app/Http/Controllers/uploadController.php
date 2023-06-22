2053ba05d3d8667aaf4a631d17101430a76ff572
ad.bme <adil.bouamrioune@gmail.com>
2023-06-22 15:59:50 +0200

    Method added for display real name of the file

---
+++ app/Http/Controllers/uploadController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class uploadController extends Controller
+{
+    public function index(Request $request)
+    {
+        $file = $request->file('file');
+        $fileName = $file->getClientOriginalName(); // Récupère le nom d'origine du fichier
+        $path = $file->storeAs('docs', $fileName); // Enregistre le fichier avec le nom d'origine dans le dossier 'docs'
+
+        return $path;
+    }
+}
